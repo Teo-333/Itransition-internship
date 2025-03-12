361edbaf642ac1c73c8b9be958d47d7c95d3b96c
Fedor Rushkevich <fedrushkevich@gmail.com>
2025-03-12 15:29:31 +0200

    (it-t-1): update solution 164b (lcs.js)

--- lcs.js
+++ lcs.js
@@ -1,1 +1,1 @@
-with(console){p=process;(s=p.argv.slice(2))[0]||p.exit();A=s[0];for(l=A.length+1;--l;)for(i=0;i<=A.length-l;i++)s.every(x=>x.includes(t=A.slice(i,i+l)))&&(log(t),p.exit())}
+p=process;a=p.argv.slice(2);a[0]||p.exit();s=a[0];for(l=s.length;l;l--)for(i=0;i+l<=s.length;i++)a.every(x=>x.includes(t=s.slice(i,i+l)))&&(console.log(t),p.exit())
