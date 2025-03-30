fcf265b130e4a453cf824798166e2c545bae0966
Fedor Rushkevich <fedrushkevich@gmail.com>
2025-03-31 02:00:48 +0300

    (it-t-4.1-p): fix UserManagmentPage(2)

--- src/frontend/services/AuthService.ts
+++ src/frontend/services/AuthService.ts
@@ -3,13 +3,13 @@ import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL 
 
 export const registerUser = async (email: string, password: string) => {
-  const response = await axios.post(`${API_URL}/register`, { email, password });
+  const response = await axios.post(`${API_URL}/auth/register`, { email, password });
   return response.data;
 };
 
 export const loginUser = async (email: string, password: string) => {
   try {
-    const response = await axios.post(`${API_URL}/login`, { email, password });
+    const response = await axios.post(`${API_URL}/auth/login`, { email, password });
     return response.data;
   } catch (error: any) {
     console.error('Login error:', error.response.data);
